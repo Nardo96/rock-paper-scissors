8f587b3291df59a2e3a759fb52fa6a491dbe2b9a
Bernardo <bernardojavier18@gmail.com>
2022-07-14 00:12:14 -0400

    Implement functional game on button click. Bug: when computer chooses rock and player chooses scissors, game result fails to get updated from last round

--- javascript.js
+++ javascript.js
@@ -1,39 +1,33 @@
 const choices = ["rock", "paper", "scissors"];
 
 function computerPlay() {
-    return choices[(Math.floor(Math.random()*choices.length))]
+  computerOutput.innerHTML = choices[(Math.floor(Math.random()*choices.length))]
 }
 
 const computerOutput = document.getElementById("computer-output")
-computerOutput.innerHTML = computerPlay();
+const userInput = document.getElementById("user-input");
 
-let userInput = document.getElementById("user-input");
-/* do playerInput = prompt("Rock, paper, or scissors?")?.toLowerCase()
-while (playerInput != "rock" 
-    && playerInput != "paper" 
-    && playerInput != "scissors"); 
-    
- //replaced by button input   */
+function setUserInput(element) {
+	userInput.innerHTML = element.id;
+	play()
+}
 
- function setUserInput(element) {
-     userInput.innerHTML = element.id;
-
- }
 const result = document.getElementById("game-result");
 function play() {
-     if (computerOutput.innerHTML == "rock"){
-         if (userInput.innerHTML == "rock") result.innerHTML = "You tied."
-         if (userInput.innerHTML == "paper") result.innerHTML = "You won."
-         if (userInput.innerHTML == "scissors") result.innerHMTL = "You lost."
-     }
-     if (computerOutput.innerHTML == "paper"){
-         if (userInput.innerHTML == "rock") result.innerHTML = "You lost."
-         if (userInput.innerHTML == "paper") result.innerHTML = "You tied."
-         if (userInput.innerHTML == "scissors") result.innerHTML = "You won!"
-     }
-     if (computerOutput.innerHTML == "scissors"){
-         if (userInput.innerHTML == "rock") result.innerHTML = "You won!"
-         if (userInput.innerHTML == "paper") result.innerHTML = "you lost."
-         if (userInput.innerHTML == "scissors") result.innerHTML = "You tied."
-     }
+	computerPlay();
+  if (computerOutput.innerHTML == "rock"){
+    if (userInput.innerHTML == "rock") result.innerHTML = "You tied."
+    if (userInput.innerHTML == "paper") result.innerHTML = "You won!"
+    if (userInput.innerHTML == "scissors") result.innerHMTL = "You lost."
+	}
+  if (computerOutput.innerHTML == "paper"){
+    if (userInput.innerHTML == "rock") result.innerHTML = "You lost."
+    if (userInput.innerHTML == "paper") result.innerHTML = "You tied."
+    if (userInput.innerHTML == "scissors") result.innerHTML = "You won!"
+  }
+  if (computerOutput.innerHTML == "scissors"){
+  	if (userInput.innerHTML == "rock") result.innerHTML = "You won!"
+    if (userInput.innerHTML == "paper") result.innerHTML = "You lost."
+    if (userInput.innerHTML == "scissors") result.innerHTML = "You tied."
+  }
 }
