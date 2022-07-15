14a6fd97052226ceacd6b0ce949df2519b37a724
Bernardo <bernardojavier18@gmail.com>
2022-07-15 15:12:02 -0400

    Replace .innerHTML with .textContent

--- javascript.js
+++ javascript.js
@@ -1,77 +1,77 @@
 const choices = ["rock", "paper", "scissors"];
 
 function computerPlay() {
-  computerOutput.innerHTML = choices[(Math.floor(Math.random()*choices.length))]
+  computerOutput.textContent = choices[(Math.floor(Math.random()*choices.length))]
 }
 
 const computerOutput = document.getElementById("computer-output")
 const userInput = document.getElementById("user-input");
 
 function setUserInput(element) {
-	userInput.innerHTML = element.id;
+	userInput.textContent = element.id;
 	play()
 }
 
 const result = document.getElementById("game-result");
 function play() {
 	computerPlay();
-  if (computerOutput.innerHTML == "rock"){
-    if (userInput.innerHTML == "rock") result.innerHTML = "You tied."
-    else if (userInput.innerHTML == "paper") result.innerHTML = "You won!"
-    else if (userInput.innerHTML == "scissors") result.innerHMTL = "You lost."
+  if (computerOutput.textContent == "rock"){
+    if (userInput.textContent == "rock") result.textContent = "You tied."
+    else if (userInput.textContent == "paper") result.textContent = "You won!"
+    else if (userInput.textContent == "scissors") result.innerHMTL = "You lost."
 	}
-  else if (computerOutput.innerHTML == "paper"){
-    if (userInput.innerHTML == "rock") result.innerHTML = "You lost."
-    else if (userInput.innerHTML == "paper") result.innerHTML = "You tied."
-    else if (userInput.innerHTML == "scissors") result.innerHTML = "You won!"
+  else if (computerOutput.textContent == "paper"){
+    if (userInput.textContent == "rock") result.textContent = "You lost."
+    else if (userInput.textContent == "paper") result.textContent = "You tied."
+    else if (userInput.textContent == "scissors") result.textContent = "You won!"
   }
-  else if (computerOutput.innerHTML == "scissors"){
-  	if (userInput.innerHTML == "rock") result.innerHTML = "You won!"
-    else if (userInput.innerHTML == "paper") result.innerHTML = "You lost."
-    else if (userInput.innerHTML == "scissors") result.innerHTML = "You tied."
+  else if (computerOutput.textContent == "scissors"){
+  	if (userInput.textContent == "rock") result.textContent = "You won!"
+    else if (userInput.textContent == "paper") result.textContent = "You lost."
+    else if (userInput.textContent == "scissors") result.textContent = "You tied."
   }
 	/* For some reason in the first if block above, when computer chooses rock and player chooses scissors, the result fails to update from the previous round. If statement below fixes this so game is fully functional. */
 
-	if (computerOutput.innerHTML == "rock" && userInput.innerHTML == "scissors") result.innerHTML = "You lost."
+	if (computerOutput.textContent == "rock" && userInput.textContent == "scissors") result.textContent = "You lost."
 }
 
 function testGame(){
 	let hasError = false;
 	for (let choice of choices){
-		userInput.innerHTML = choice;
+		userInput.textContent = choice;
 		let compPlayedRock = false;
 		let compPlayedPaper = false;
 		let compPlayedScissors = false;
 		while (!compPlayedRock || !compPlayedPaper || !compPlayedScissors){
 			play();
-			if (computerOutput.innerHTML == "rock"){
+			if (computerOutput.textContent == "rock"){
 				compPlayedRock = true;
 				if (choice == "rock"){
-					if (!(result.innerHTML == "You tied.")) hasError = true;
+					if (!(result.textContent == "You tied.")) hasError = true;
 				} else if(choice == "paper"){
-					if (!(result.innerHTML == "You won!")) hasError = true;
+					if (!(result.textContent == "You won!")) hasError = true;
 				} else if (choice == "scissors"){
-					if (!(result.innerHTML == "You lost.")) hasError = true;
+					if (!(result.textContent == "You lost.")) hasError = true;
 				}
 			}
-			else if (computerOutput.innerHTML == "paper"){
+			else if (computerOutput.textContent == "paper"){
 				compPlayedPaper = true;
 				if (choice == "rock"){
-					if (!(result.innerHTML == "You lost.")) hasError = true;
+					if (!(result.textContent == "You lost.")) hasError = true;
 				} else if (choice == "paper"){
-					if (!(result.innerHTML == "You tied.")) hasError = true;
+					if (!(result.textContent == "You tied.")) hasError = true;
 				} else if (choice == "scissors"){
-					if (!(result.innerHTML == "You won!")) hasError = true;
+					if (!(result.textContent == "You won!")) hasError = true;
 				}
 			}
-			else if (computerOutput.innerHTML == "scissors"){
+			else if (computerOutput.textContent == "scissors"){
 				compPlayedScissors = true;
 				if (choice == "rock"){
-					if (!(result.innerHTML == "You won!")) hasError = true;
+					if (!(result.textContent == "You won!")) hasError = true;
 				} else if (choice == "paper"){
-					if (!(result.innerHTML == "You lost.")) hasError = true;
+					if (!(result.textContent == "You lost.")) hasError = true;
 				} else if (choice == "scissors"){
-					if (!(result.innerHTML == "You tied.")) hasError = true;
+					if (!(result.textContent == "You tied.")) hasError = true;
 				}
 			}
 		}
